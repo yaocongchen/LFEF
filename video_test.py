8082fdb58b87aecdbbb0fea5c53ace646b652505
yaocongchen <yaocongchen@outlook.com>
2023-04-07 18:32:20 +0800

    202304007-2

--- video_test.py
+++ video_test.py
@@ -1,7 +1,13 @@
 import cv2
 
-cap = cv2.VideoCapture('/home/yaocong/Experimental/speed_smoke_segmentation/smoke_video.mp4')
+cap = cv2.VideoCapture(0)
+# 設定擷取影像的尺寸大小
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+print(cv2.getBuildInformation())
 #Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('./output.avi', fourcc, 30.0, (1280,720),3)
 
 while cap.isOpened():
     ret,frame = cap.read()
@@ -9,10 +15,18 @@ while cap.isOpened():
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
+    #水平上下反轉影像
+    #frame = cv2.flip(frame,0)
+    #write the flipped frame
+    
     #gray = cv2.cvtColor(frame,cv2.COLOR_BGRA2GRAY)
     cv2.imshow('frame',frame)
+    out.write(frame)
+
     if cv2.waitKey(1) == ord('q'):
         break
-
+    
+#Release everything if job is finished
 cap.release()
+out.release()
 cv2.destroyAllWindows
