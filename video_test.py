e6177989cc9111f5b86b9c9f5d8aed69190e0a40
yaocongchen <yaocongchen@outlook.com>
2023-04-10 13:40:48 +0800

    check_ok

--- video_test.py
+++ video_test.py
@@ -1,13 +1,13 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('/home/yaocong/Experimental/speed_smoke_segmentation/smoke_video.mp4')
 # 設定擷取影像的尺寸大小
 #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-print(cv2.getBuildInformation())
+#print(cv2.getBuildInformation())
 #Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('./output.avi', fourcc, 30.0, (1280,720),3)
+out = cv2.VideoWriter('./output.mp4', fourcc, 30.0, (1280,720),3)
 
 while cap.isOpened():
     ret,frame = cap.read()
