a6349452460c13e8af18ab77d585d7c6a8db2f1b
yaocongchen <yaocongchen@outlook.com>
2023-04-19 15:24:05 +0800

    tidy test.py

--- test.py
+++ test.py
@@ -10,22 +10,25 @@ import utils
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 
-device = (torch.device('cuda') if torch.cuda.is_available()
-        else torch.device('cpu'))
-#print(f"Training on device {device}.")
+def folders_and_files_name():
+    # Set save folder and file name 設定存檔資料夾與存檔名稱  
+    save_smoke_semantic_dir_name = "multiple_result"
+    if os.path.exists("./" + save_smoke_semantic_dir_name):
+        shutil.rmtree("./" + save_smoke_semantic_dir_name)      # Delete the original folder and content 將原有的資料夾與內容刪除
+        os.makedirs("./" + save_smoke_semantic_dir_name)        # Create new folder 創建新的資料夾
+    else:
+    # if not os.path.exists("./" + save_smoke_semantic_dir_name):
+        os.makedirs("./" + save_smoke_semantic_dir_name)
+    save_smoke_semantic_image_name = "smoke_semantic_image_"
 
-# Set save folder and file name 設定存檔資料夾與存檔名稱  
-save_smoke_semantic_dir_name = "multiple_result"
-if os.path.exists("./" + save_smoke_semantic_dir_name):
-    shutil.rmtree("./" + save_smoke_semantic_dir_name)      # Delete the original folder and content 將原有的資料夾與內容刪除
-    os.makedirs("./" + save_smoke_semantic_dir_name)        # Create new folder 創建新的資料夾
-else:
-# if not os.path.exists("./" + save_smoke_semantic_dir_name):
-    os.makedirs("./" + save_smoke_semantic_dir_name)
-save_smoke_semantic_image_name = "smoke_semantic_image_"
+    names = {}
+    names["smoke_semantic_dir_name"] = save_smoke_semantic_dir_name
+    names["smoke_semantic_image_name"] = save_smoke_semantic_image_name
+
+    return names
 
 # Main function 主函式 
-def smoke_segmentation(args):
+def smoke_segmentation(device,names):
     testing_data = utils.dataset.DataLoaderSegmentation(args['test_images'],
                                                 args['test_masks'],mode = 'test')
     testing_data_loader = DataLoader(testing_data ,batch_size= args['batch_size'], shuffle = True, num_workers =args['num_workers'], pin_memory = True, drop_last=True)
@@ -36,13 +39,12 @@ def smoke_segmentation(args):
         img_image = img_image.to(device)
         mask_image = mask_image.to(device)
 
-        output = smoke_semantic(img_image,args['model_path'])
+        output = smoke_semantic(img_image,args['model_path'],device)
         count += 1
-        torchvision.utils.save_image(torch.cat((mask_image,output),0),"./" + save_smoke_semantic_dir_name + "/" + save_smoke_semantic_image_name  + f"{count}.jpg")
+        torchvision.utils.save_image(torch.cat((mask_image,output),0),"./" + names["smoke_semantic_dir_name"] + "/" + names["smoke_semantic_image_name"]  + f"{count}.jpg")
 
         loss = utils.loss.CustomLoss(output, mask_image)
         acc = utils.metrics.acc_miou(output,mask_image)
-
 
         pbar.set_postfix(test_loss=loss.item(),test_acc=acc.item())
 
@@ -59,9 +61,14 @@ if __name__ == "__main__":
     ap.add_argument('-m','--model_path' ,required=True, help="load model path")
     args = vars(ap.parse_args())
 
+    device = (torch.device('cuda') if torch.cuda.is_available()
+        else torch.device('cpu'))
+    #print(f"Training on device {device}.")
+
+    names=folders_and_files_name()
     # Calculate the total implement time 計算總執行時間
     time_start = time.time()
-    smoke_segmentation(args)
+    smoke_segmentation(device,names)
     time_end = time.time()
     total_image = len(os.listdir(args["test_images"]))
     spend_time = int(time_end-time_start) 
