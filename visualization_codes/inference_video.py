53269d3f192fadd71a000056b5de464f0d51a5cb
yaocongchen <yaocongchen@outlook.com>
2023-05-23 16:15:10 +0800

    rename folder

--- visualization_codes/inference_video.py
+++ visualization_codes/inference_video.py
@@ -1,7 +1,7 @@
 import cv2
 from visualization_codes.inference import smoke_semantic
 # import visualization_codes.image_process_utils as image_process
-import visualization_codes.testcpy.image_process_utils_cython as image_process
+import visualization_codes.process_utils_cython_version.image_process_utils_cython as image_process
 import torch
 import argparse
 import time
