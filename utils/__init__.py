3c9c86e51ddfa85bbdd77cd87435beeaa57f95f4
yaocongchen <yaocongchen@outlook.com>
2023-09-22 18:35:53 +0800

    add desmoke #python & drawsio

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,24 @@
 # init
 import utils.dataset
 import utils.dataset_smoke120k
-import utils.dataset_for_test
+import utils.dataset_for_test  # don't del
+import utils.dataset_smoke120k_desmoke
+import utils.dataset_smoke120k_seg_desmoke
+
 import utils.load_npy_dataset
 import utils.loss
 import utils.two_loss
+import utils.loss_desmoke
 import utils.metrics
 
 dataset = utils.dataset
 dataset_smoke120k = utils.dataset_smoke120k
 dataset_for_test = utils.dataset_for_test
+dataset_smoke120k_desmoke = utils.dataset_smoke120k_desmoke
+dataset_smoke120k_seg_desmoke = utils.dataset_smoke120k_seg_desmoke
+
 load_npy_dataset = utils.load_npy_dataset
 loss = utils.loss
 two_loss = utils.two_loss
+loss_desmoke = utils.loss_desmoke
 metrics = utils.metrics
