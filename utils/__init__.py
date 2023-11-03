31d72305a3024dd629120fc4397241bfabcf546f
yaocongchen <yaocongchen@outlook.com>
2023-11-03 12:24:46 +0800

    Commented out unused imports in utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,11 @@
 # init
 import utils.dataset
 import utils.dataset_smoke100k
-import utils.dataset_for_test_old  # don't del
+#import utils.dataset_for_test_old  # don't del
 import utils.dataset_smoke100k_desmoke
 import utils.dataset_smoke100k_seg_desmoke
 
-import utils.load_npy_dataset
+#import utils.load_npy_dataset
 import utils.loss
 import utils.two_loss
 import utils.loss_desmoke
