fc2f91b6c412fd28b36df4225be39050e1ff6de4
yaocongchen <yaocongchen@outlook.com>
2023-11-09 14:37:01 +0800

    Add SSIM to CustomLoss function

--- utils/loss.py
+++ utils/loss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from skimage.metrics import structural_similarity
 
 alpha = 0.5
 lambda_reg = 0.01
@@ -65,14 +66,40 @@ def IoU(
         (intersection + smooth) / (union + smooth), dim=0
     )  # 2*考慮重疊的部份 #計算模型輸出和真實標籤的Dice係數，用於評估二元分割模型的性能。參數model_output和mask分別為模型輸出和真實標籤，smooth是一個常數，用於避免分母為0的情況。
 
+def SSIM(model_output, mask):
+    output_np = (
+        model_output.squeeze()
+        .mul(255)
+        .add_(0.5)
+        .clamp_(0, 255)
+        .contiguous()
+        .to("cpu")
+        .detach()
+        .numpy()
+    )
 
+    np.set_printoptions(threshold=np.inf)
+    output_np[output_np >= 1] = 1
+    # output_np[1< output_np] = 0
+
+    # model_output = torch.from_numpy(output_np).to("cuda")
+
+    mask = mask.squeeze().contiguous().to("cpu").detach().numpy()
+    # Compute SSIM between two images
+    (score, diff) = structural_similarity(output_np, mask, data_range=1, full=True)
+    # print("Image similarity", score)
+
+    return score
 def CustomLoss(model_output, mask,device):
     # s_iou = Sigmoid_IoU(model_output,mask)
     iou = IoU(model_output,mask,device)
 
+    ssim = SSIM(model_output,mask)
+
     loss_1 = L(S(model_output), mask)
 
-    total_loss = loss_1 * (1 - alpha) + (1 - iou) * alpha
+
+    total_loss = loss_1 * (1 - alpha) + (1 - iou) * (alpha/2) + (1 - ssim) * (alpha/2)
     # total_loss = loss_1
 
     return total_loss
