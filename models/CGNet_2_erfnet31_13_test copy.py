2cafdae8f6c0c50b37d591c6adc72ead969c40b8
yaocongchen <yaocongchen@outlook.com>
2023-11-09 11:50:24 +0800

    Fix classifier output in CGNet_2_erfnet31_13_test.py

--- models/CGNet_2_erfnet31_13_test copy.py
+++ models/CGNet_2_erfnet31_13_test copy.py
@@ -562,14 +562,21 @@ class Net(nn.Module):
         output2_cat_sem_cam = output2_cat * sem_cam
 
         # classifier
-        classifier = self.classifier(output2_cat_sem_cam)
+        classifier = self.classifier(output2_cat)
         
         # upsample segmenation map ---> the input image size
         out = F.interpolate(
             classifier, input.size()[2:], mode="bilinear", align_corners=False
         )  # Upsample score map, factor=8
                 # classifier
-        return out
+
+        classifier2 = self.classifier(output2_cat_sem_cam)
+        
+        # upsample segmenation map ---> the input image size
+        out2 = F.interpolate(
+            classifier2, input.size()[2:], mode="bilinear", align_corners=False
+        )  # Upsample score map, factor=8
+        return out ,out2
 
 
 if __name__ == "__main__":
