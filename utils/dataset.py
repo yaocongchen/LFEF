12c980d7d108db187f0f51ebd13ff7adac0bbc60
yaocongchen <yaocongchen@outlook.com>
2023-11-24 10:56:02 +0800

    Adjust brightness augmentation factor

--- utils/dataset.py
+++ utils/dataset.py
@@ -18,7 +18,7 @@ def cv2_brightness_augment(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     v = hsv[:, :, 2]
     # seed = random.uniform(0.5, 1.2)
-    v = ((v / 255.0) * 1.5) * 255.0
+    v = ((v / 255.0) * 1.2) * 255.0
     hsv[:, :, 2] = np.array(np.clip(v, 0, 255), dtype=np.uint8)
     rgb_final = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
     return rgb_final
