Fix Sigmoid function in CGNet_2_erfnet31_13_3113_dilated.py and update loss calculation in loss.py

models/CGNet_2_erfnet31_13_3113_dilated.py
@@ -499,7 +499,7 @@ class Net(nn.Module):
                     if m.bias is not None:
                         m.bias.data.zero_()
         
-        self.my_simgoid = nn.Sigmoid()
+        # self.my_simgoid = nn.Sigmoid()
 
     def forward(self, input):
         """