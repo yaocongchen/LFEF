Fix SSIM calculation in loss and metrics modules

utils/metrics.py
@@ -85,8 +85,8 @@ def IoU(
     )  # 2*考慮重疊的部份 #計算模型輸出和真實標籤的Dice係數，用於評估二元分割模型的性能。參數model_output和mask分別為模型輸出和真實標籤，smooth是一個常數，用於避免分母為0的情況。
 
 def ssim_val(model_output, mask):
-    # model_output = S(model_output)
-    # output_np = (
+
+    # model_output = (
     #     model_output.squeeze()
     #     .mul(255)
     #     .add_(0.5)
@@ -97,29 +97,28 @@ def ssim_val(model_output, mask):
     #     .numpy()
     # )
 
-    # # np.set_printoptions(threshold=np.inf)
-    # # output_np[output_np >= 1] = 1
-
-    # # output_np[1< output_np] = 0
-
-    # # # model_output = torch.from_numpy(output_np).to("cuda")
-
+    # model_output = (model_output > 0.5)
+    # # print("model_output",model_output)
     # mask = (mask.squeeze()
-    #     .mul(255)
-    #     .add_(0.5)
-    #     .clamp_(0, 255)
     #     .contiguous()
     #     .to("cpu")
     #     .detach()
     #     .numpy()
     # )
+    # # print("mask",mask)
     # # # Compute SSIM between two images
-    # msssim, grad,s = structural_similarity(output_np, mask, gradient=True,data_range=1, full=True)
+    # msssim = structural_similarity(model_output, mask,data_range=1,win_size=11,win_sigma=1.5,size_average=True,k1=0.01,k2=0.03,gaussian_weights=True)
 
-    #model_output = S(model_output)
-    msssim = ssim(model_output, mask, data_range=1, size_average=True)
+
+    model_output = (
+        model_output.mul(255)
+        .add_(0.5)
+        .clamp_(0, 255)
+    )
+
+    model_output = (model_output > 0.5).float()
+    msssim = ssim(model_output, mask, data_range=1)
     
-    # print("Image similarity", score)
     return msssim
 
 