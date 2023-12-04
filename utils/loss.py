f1b5c3924c236870bb285885b1aaf75f96d71b7c
yaocongchen <yaocongchen@outlook.com>
2023-12-04 11:07:01 +0800

    Update model structure diagram and loss function

--- utils/loss.py
+++ utils/loss.py
@@ -124,8 +124,8 @@ def CustomLoss(model_output, mask,device):
     loss_1 = L(S(model_output), mask)
 
 
-    total_loss = loss_1 * (1 - alpha) + (1 - iou) * (alpha/2) + (1 - my_ssim) * (alpha/2)
-    # total_loss = loss_1 * (1 - alpha) + (1 - iou) * (alpha)
+    # total_loss = loss_1 * (1 - alpha) + (1 - iou) * (alpha/2) + (1 - my_ssim) * (alpha/2)
+    total_loss = loss_1 * (1 - alpha) + (1 - iou) * (alpha)
     # total_loss = loss_1
 
     return total_loss
