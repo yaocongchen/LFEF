d38af9c836232d012604438c8efa211dc07dccc8
yaocongchen <yaocongchen@outlook.com>
2023-12-25 12:01:33 +0800

    Update CGNet model structure and training script

--- models/CGNet_2_erfnet31_13_3113_oneloss.py
+++ models/CGNet_2_erfnet31_13_3113_oneloss.py
@@ -232,9 +232,9 @@ class ChannelWiseDilatedConv(nn.Module):
             nn.Conv2d(
                 nIn,
                 nIn,
-                (1, kSize),
+                (kSize, 1),
                 stride=stride,
-                padding=(0, padding),
+                padding=(padding , 0),
                 groups=nIn,
                 bias=False,
                 dilation=d,
@@ -242,9 +242,9 @@ class ChannelWiseDilatedConv(nn.Module):
             nn.Conv2d(
                 nIn,
                 nOut,
-                (kSize, 1),
+                (1, kSize),
                 stride=stride,
-                padding=(padding, 0),
+                padding=(0 , padding),
                 groups=nIn,
                 bias=False,
                 dilation=d,
