aceb623060bfe874a3622af4425403b86e68070e
yaocongchen <yaocongchen@outlook.com>
2023-12-28 09:54:26 +0800

    Add flip text and image functionality to app.py

--- app.py
+++ app.py
@@ -2,16 +2,32 @@
 import numpy as np
 import gradio as gr
 
-def sepia(input_img):
-    sepia_filter = np.array([
-        [0.393, 0.769, 0.189], 
-        [0.349, 0.686, 0.168], 
-        [0.272, 0.534, 0.131]
-    ])
-    sepia_img = input_img.dot(sepia_filter.T)
-    sepia_img /= sepia_img.max()
-    return sepia_img
 
-demo = gr.Interface(sepia, gr.Image(), "image")
-demo.launch()  
+def flip_text(x):
+    return x[::-1]
+
+
+def flip_image(x):
+    return np.fliplr(x)
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("Flip text or image files using this demo.")
+    with gr.Tab("Flip Text"):
+        text_input = gr.Textbox()
+        text_output = gr.Textbox()
+        text_button = gr.Button("Flip")
+    with gr.Tab("Flip Image"):
+        with gr.Row():
+            image_input = gr.Image()
+            image_output = gr.Image()
+        image_button = gr.Button("Flip1")
+
+    with gr.Accordion("Open for More!"):
+        gr.Markdown("Look at me...")
+
+    text_button.click(flip_text, inputs=text_input, outputs=text_output)
+    image_button.click(flip_image, inputs=image_input, outputs=image_output)
+
+demo.launch()
 #%%
