d4b206b7d613950ee79a07f1c2c61a89bbb7214f
yaocongchen <yaocongchen@outlook.com>
2024-02-24 14:52:20 +0800

    Refactor total_loss calculation in CustomLoss function

--- utils/loss.py
+++ utils/loss.py
@@ -125,7 +125,7 @@ def CustomLoss(model_output, mask):
 
 
     # total_loss = loss_1 * (1 - alpha) + (1 - iou) * (alpha/2) + (1 - my_ssim) * (alpha/2)
-    total_loss = loss_1 + (1 - iou) * (alpha)
-    # total_loss = loss_1
+    # total_loss = loss_1 * (1 - alpha) + (1 - iou) * (alpha)
+    total_loss = loss_1
 
     return total_loss
