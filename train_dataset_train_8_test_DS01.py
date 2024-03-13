8b18a59c95279bbe89b20cbd11a0af4607f71a89
yaocongchen <yaocongchen@outlook.com>
2024-03-13 17:42:28 +0800

    Add weight decay to Adam optimizer and define a learning rate scheduler

--- train_dataset_train_8_test_DS01.py
+++ train_dataset_train_8_test_DS01.py
@@ -307,10 +307,10 @@ def main():
 
     # 先用Adam測試模型能力
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=float(args["learning_rate"])
+        model.parameters(), lr=float(args["learning_rate"]), weight_decay=float(args["weight_decay"])
     )
-    # # 定義一個當訓練損失停止下降時就將學習率減半的調度器
-    # scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=10, factor=0.5)
+    # 定義一個當訓練損失停止下降時就將學習率減半的調度器
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=10, factor=0.5)
 
     # scheduler = StepLR(optimizer, step_size=1, gamma=0.95)
     # 創建一個學習率排程
