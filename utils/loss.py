Update alpha value and fix boundary loss calculation

utils/loss.py
@@ -5,7 +5,7 @@ import numpy as np
 from skimage.metrics import structural_similarity
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 
-alpha = 0.2
+alpha = 0.5
 lambda_reg = 0.2
 
 S = nn.Sigmoid()
@@ -125,7 +125,7 @@ def dice_coef(model_output, mask):
 
 def boundary_loss(pred, target):
 
-    pred = (pred > 0.5).float()
+
     # 計算邊界
     pred_boundary = F.max_pool2d(1 - pred, kernel_size=3, stride=1, padding=1) - (1 - pred)
     target_boundary = F.max_pool2d(1 - target, kernel_size=3, stride=1, padding=1) - (1 - target)
@@ -136,6 +136,7 @@ def boundary_loss(pred, target):
     return boundary_loss
 
 def CustomLoss(model_output, mask):
+    model_output = (model_output > 0.5).float()
     # s_iou = Sigmoid_IoU(model_output,mask)
     # iou = IoU(model_output,mask)
 