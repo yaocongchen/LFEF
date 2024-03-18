0199ae5745192b42250c636666b36de0b72bc876
yaocongchen <yaocongchen@outlook.com>
2024-03-18 14:01:24 +0800

    Fix bug in login functionality

--- models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
+++ models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchinfo import summary
 from torch.nn import init
+import torchvision.transforms.functional as TF
 
 __all__ = ["Context_Guided_Network"]
 # Filter out variables, functions, and classes that other programs don't need or don't want when running cmd "from CGNet import *"
@@ -571,7 +572,7 @@ class Net(nn.Module):
     This class defines the proposed Context Guided Network (CGNet) in this work.
     """
 
-    def __init__(self, classes=1, M=3, N=3, dropout_flag=False):
+    def __init__(self, classes=1, M=3, N=3, dropout_flag=False, rotation_angle=180):
         """
         args:
           classes: number of classes in the dataset. Default is 19 for the cityscapes
@@ -591,6 +592,7 @@ class Net(nn.Module):
 
         self.aux_net = AuxiliaryNetwork(3, 32, stride = 2)
 
+        self.rotation_angle = rotation_angle
 
         # stage 2
         self.level2_0 = ContextGuidedBlock_Down(
@@ -604,7 +606,7 @@ class Net(nn.Module):
         self.in_relu_2 = INReLU(128)
         # self.bn_relu_2_2 = BNReLU(128 + 3)
 
-
+        
         # stage 3
         self.level3_0 = ContextGuidedBlock_Down(
             128, 128, dilation_rate=4, reduction=16
@@ -645,14 +647,14 @@ class Net(nn.Module):
         # self.max_pool = nn.MaxPool2d(3, stride=1, padding=1)
         # self.conv_256_to_128 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=(1, 1), padding=0), nn.ReLU())
                         
-        self.upsample_64x64 = nn.Upsample(size=(64, 64), mode="bilinear", align_corners=True)
-        self.conv_32_to_32 = nn.Sequential(nn.Conv2d(32, 32, kernel_size=(1, 1), padding=0,stride=2,groups=1), nn.InstanceNorm2d(32, affine = True), nn.ReLU())
-        self.conv_128_to_64 = nn.Sequential(nn.Conv2d(128, 64, kernel_size=(1, 1), padding=0,groups=1), nn.InstanceNorm2d(64, affine = True), nn.ReLU())
-        self.conv_256_to_128 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=(1, 1), padding=0,groups=1), nn.InstanceNorm2d(128, affine = True), nn.ReLU())
+        # self.upsample_64x64 = nn.Upsample(size=(64, 64), mode="bilinear", align_corners=True)
+        # self.conv_32_to_32 = nn.Sequential(nn.Conv2d(32, 32, kernel_size=(1, 1), padding=0,stride=2,groups=1), nn.InstanceNorm2d(32, affine = True), nn.ReLU())
+        # self.conv_128_to_64 = nn.Sequential(nn.Conv2d(128, 64, kernel_size=(1, 1), padding=0,groups=1), nn.InstanceNorm2d(64, affine = True), nn.ReLU())
+        # self.conv_256_to_128 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=(1, 1), padding=0,groups=1), nn.InstanceNorm2d(128, affine = True), nn.ReLU())
 
-        self.conv3x3_224_to_64 = nn.Sequential(nn.Conv2d(224, 64, kernel_size=(3, 3), padding=1,groups=1), nn.InstanceNorm2d(64, affine = True), nn.ReLU())
-        self.upsample_256x256 = nn.Upsample(size=(256, 256), mode="bilinear", align_corners=True)
-        self.conv_64_to_1 = nn.Conv2d(64, 1, kernel_size=(1, 1), padding=0,groups=1)
+        # self.conv1x1_224_to_64 = nn.Sequential(nn.Conv2d(224, 64, kernel_size=(1, 1), padding=1,groups=1), nn.InstanceNorm2d(64, affine = True), nn.ReLU())
+        # self.upsample_256x256 = nn.Upsample(size=(256, 256), mode="bilinear", align_corners=True)
+        # self.conv_64_to_1 = nn.Conv2d(64, 1, kernel_size=(1, 1), padding=0,groups=1)
                         
 #================================================================================================#
         
@@ -662,19 +664,19 @@ class Net(nn.Module):
         # self.conv_256_to_1 = nn.Sequential(nn.Conv2d(256, 1, kernel_size=(1, 1), padding=0,groups=1), nn.InstanceNorm2d(1, affine = True), nn.ReLU())
 
 #================================================================================================#
-        # self.conv_256_to_128 = nn.Conv2d(256, 128, kernel_size=(1, 1), stride=1,padding=0)
-        # self.conv_256_to_128_IN = nn.InstanceNorm2d(128, affine=True)
-        # self.upsample_to_64x64 = nn.Upsample(size=(64, 64), mode="bilinear", align_corners=True)
+        self.conv_256_to_128 = nn.Conv2d(256, 128, kernel_size=(1, 1), stride=1,padding=0)
+        self.conv_256_to_128_IN = nn.InstanceNorm2d(128, affine=True)
+        self.upsample_to_64x64 = nn.Upsample(size=(64, 64), mode="bilinear", align_corners=True)
 
-        # self.conv_256_to_32 = nn.Conv2d(256, 32, kernel_size=(1, 1), stride=1,padding=0)
-        # self.conv_256_to_32_IN = nn.InstanceNorm2d(32, affine=True)
-        # self.upsample_to_128x128 = nn.Upsample(size=(128, 128), mode="bilinear", align_corners=True)
+        self.conv_256_to_32 = nn.Conv2d(256, 32, kernel_size=(1, 1), stride=1,padding=0)
+        self.conv_256_to_32_IN = nn.InstanceNorm2d(32, affine=True)
+        self.upsample_to_128x128 = nn.Upsample(size=(128, 128), mode="bilinear", align_corners=True)
 
-        # self.conv_96_to_1 = nn.Conv2d(64, 1, kernel_size=(1, 1), stride=1,padding=0)
-        # self.conv_96_to_1_IN = nn.InstanceNorm2d(1, affine=True)
-        # self.upsample_to_256x256 = nn.Upsample(size=(256, 256), mode="bilinear", align_corners=True)
+        self.conv_96_to_1 = nn.Conv2d(64, 1, kernel_size=(1, 1), stride=1,padding=0)
+        self.conv_96_to_1_IN = nn.InstanceNorm2d(1, affine=True)
+        self.upsample_to_256x256 = nn.Upsample(size=(256, 256), mode="bilinear", align_corners=True)
 
-        # self.relu = nn.ReLU()
+        self.relu = nn.ReLU()
 #================================================================================================#
         
         self.sigmoid = nn.Sigmoid()
@@ -697,9 +699,10 @@ class Net(nn.Module):
         inverted_output = self.aux_net(input_inverted)
         stage1_ewp_inverted_output = stage1_output * inverted_output
 
-
+        
+        stage1_ewp_inverted_output_rotated = TF.rotate(stage1_ewp_inverted_output, self.rotation_angle)
         # stage 2
-        initial_stage2_output = self.level2_0(stage1_ewp_inverted_output)  # down-sampled
+        initial_stage2_output = self.level2_0(stage1_ewp_inverted_output_rotated)  # down-sampled
 
         for i, layer in enumerate(self.level2):
             if i == 0:
@@ -708,6 +711,8 @@ class Net(nn.Module):
                 processed_stage2_output = layer(processed_stage2_output)
 
         final_stage2_output = self.in_relu_2(torch.cat([initial_stage2_output, processed_stage2_output], 1))
+
+        final_stage2_output = TF.rotate(final_stage2_output, -self.rotation_angle)
 
 
         # b, c, w, h = initial_stage2_output.size()
@@ -732,18 +737,18 @@ class Net(nn.Module):
 
         final_stage3_output = self.in_relu_3(torch.cat([initial_stage3_output, processed_stage3_output], 1))
 
-        final_stage3_output_up = self.upsample_64x64(final_stage3_output)
-        final_stage3_output_conv = self.conv_256_to_128(final_stage3_output_up)
+        # final_stage3_output_up = self.upsample_64x64(final_stage3_output)
+        # final_stage3_output_conv = self.conv_256_to_128(final_stage3_output_up)
 
-        final_stage2_output_conv = self.conv_128_to_64(final_stage2_output)
+        # final_stage2_output_conv = self.conv_128_to_64(final_stage2_output)
 
-        stage1_ewp_inverted_output_conv = self.conv_32_to_32(stage1_ewp_inverted_output)
+        # stage1_ewp_inverted_output_conv = self.conv_32_to_32(stage1_ewp_inverted_output)
 
 
-        output = torch.cat([final_stage3_output_conv, final_stage2_output_conv, stage1_ewp_inverted_output_conv], 1)
-        output = self.conv3x3_224_to_64(output)
-        output = self.upsample_256x256(output)
-        output = self.conv_64_to_1(output)
+        # output = torch.cat([final_stage3_output_conv, final_stage2_output_conv, stage1_ewp_inverted_output_conv], 1)
+        # output = self.conv1x1_224_to_64(output)
+        # output = self.upsample_256x256(output)
+        # output = self.conv_64_to_1(output)
 
 #================================================================================================#
         # stage1_ewp_inverted_output_up = self.upsample(stage1_ewp_inverted_output)
@@ -759,25 +764,25 @@ class Net(nn.Module):
         # output = self.classifier(output)
 
 #================================================================================================#
-        # upsample_stage3_output = self.upsample_to_64x64(final_stage3_output)
-        # convolved_stage3_output = self.conv_256_to_128(upsample_stage3_output)
-        # convolved_stage3_output = self.conv_256_to_128_IN(convolved_stage3_output)
-        # # convolved_stage3_output = F.layer_norm(convolved_stage3_output, convolved_stage3_output.size()[1:])
-        # convolved_stage3_output = self.relu(convolved_stage3_output)
+        upsample_stage3_output = self.upsample_to_64x64(final_stage3_output)
+        convolved_stage3_output = self.conv_256_to_128(upsample_stage3_output)
+        convolved_stage3_output = self.conv_256_to_128_IN(convolved_stage3_output)
+        # convolved_stage3_output = F.layer_norm(convolved_stage3_output, convolved_stage3_output.size()[1:])
+        convolved_stage3_output = self.relu(convolved_stage3_output)
 
-        # stage3_cat_stage2_output = torch.cat([convolved_stage3_output, final_stage2_output], 1)
-        # upsample_stage2_output = self.upsample_to_128x128(stage3_cat_stage2_output)
-        # convolved_stage2_output = self.conv_256_to_32(upsample_stage2_output)
-        # convolved_stage2_output = self.conv_256_to_32_IN(convolved_stage2_output)
-        # # convolved_stage2_output = F.layer_norm(convolved_stage2_output, convolved_stage2_output.size()[1:])
-        # convolved_stage2_output = self.relu(convolved_stage2_output)
+        stage3_cat_stage2_output = torch.cat([convolved_stage3_output, final_stage2_output], 1)
+        upsample_stage2_output = self.upsample_to_128x128(stage3_cat_stage2_output)
+        convolved_stage2_output = self.conv_256_to_32(upsample_stage2_output)
+        convolved_stage2_output = self.conv_256_to_32_IN(convolved_stage2_output)
+        # convolved_stage2_output = F.layer_norm(convolved_stage2_output, convolved_stage2_output.size()[1:])
+        convolved_stage2_output = self.relu(convolved_stage2_output)
 
-        # stage2_cat_stage1_output = torch.cat([convolved_stage2_output, stage1_output], 1)
-        # upsample_stage1_output = self.upsample_to_256x256(stage2_cat_stage1_output)
-        # convolved_stage1_output = self.conv_96_to_1(upsample_stage1_output)
-        # convolved_stage1_output = self.conv_96_to_1_IN(convolved_stage1_output)
-        # # convolved_stage1_output = F.layer_norm(convolved_stage1_output, convolved_stage1_output.size()[1:])
-        # convolved_stage1_output = self.relu(convolved_stage1_output)
+        stage2_cat_stage1_output = torch.cat([convolved_stage2_output, stage1_output], 1)
+        upsample_stage1_output = self.upsample_to_256x256(stage2_cat_stage1_output)
+        convolved_stage1_output = self.conv_96_to_1(upsample_stage1_output)
+        convolved_stage1_output = self.conv_96_to_1_IN(convolved_stage1_output)
+        # convolved_stage1_output = F.layer_norm(convolved_stage1_output, convolved_stage1_output.size()[1:])
+        output = self.relu(convolved_stage1_output)
 #================================================================================================#
 
         output = self.sigmoid(output)
