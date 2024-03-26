767722fd71d5d950400127519db7e2e8dec26d32
yaocongchen <yaocongchen@outlook.com>
2024-03-26 14:39:30 +0800

    Add contrastive loss function and update VGG model

--- utils/loss.py
+++ utils/loss.py
@@ -4,14 +4,13 @@ import torch.nn.functional as F
 import numpy as np
 from skimage.metrics import structural_similarity
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
-from pytorch_metric_learning import losses
+import torchvision.models as models
 
 alpha = 0.2
 lambda_reg = 0.2
 
 S = nn.Sigmoid()
 L = nn.BCELoss(reduction="mean")
-ContrastiveLoss = losses.ContrastiveLoss()
 
 
 # def Sigmoid_IoU(
@@ -137,6 +136,28 @@ def boundary_loss(pred, target):
     
     return boundary_loss
 
+def l2_normalize(x, dim=None, epsilon=1e-12):
+        square_sum = torch.sum(torch.square(x), dim=dim, keepdims=True)
+        x_inv_norm = torch.rsqrt(torch.maximum(square_sum, torch.ones_like(square_sum) * epsilon))
+        return torch.multiply(x, x_inv_norm)
+
+def contrastive_loss(image_feat, cond_feat, temperature=0.07):
+    """Calculates contrastive loss."""
+    image_feat = F.normalize(image_feat, -1)
+    cond_feat = l2_normalize(cond_feat, -1)
+    
+    bs = image_feat.shape[0]
+    labels = torch.arange(bs, device=image_feat.device)
+
+    logits_img2cond = torch.matmul(image_feat, cond_feat.t()) / temperature
+    logits_cond2img = torch.matmul(cond_feat, image_feat.t()) / temperature
+    loss_img2cond = F.cross_entropy(logits_img2cond, labels)
+    loss_cond2img = F.cross_entropy(logits_cond2img, labels)
+    loss_img2cond = torch.mean(loss_img2cond)
+    loss_cond2img = torch.mean(loss_cond2img)
+    loss = loss_img2cond + loss_cond2img
+    return loss
+
 def CustomLoss(model_output, mask):
     # s_iou = Sigmoid_IoU(model_output,mask)
     # iou = IoU(model_output,mask)
@@ -147,10 +168,27 @@ def CustomLoss(model_output, mask):
 
     loss_1 = L(model_output, mask)
 
-    model_output = model_output.flatten()
-    mask = mask.flatten()
-    loss_2 = ContrastiveLoss(model_output, mask)
+    model = models.vgg16(pretrained=True)
 
+
+
+    model_output = model_output.cuda()
+    mask = mask.cuda()
+
+
+    model.features[0] = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1) 
+    model = model.cuda()
+    with torch.no_grad():
+    # 这里执行模型的推理或评估操作，梯度不会被计算
+        model_output = model(model_output)
+        mask = model(mask)
+
+    print(model_output.shape)
+    print(mask.shape)
+    # model_output = torch.sum(model_output, dim=[2, 3])
+    # mask = torch.sum(mask, dim=[2, 3])
+
+    loss_2 = contrastive_loss(model_output, mask)
 
     # loss_2 = boundary_loss(model_output, mask)
 
@@ -160,3 +198,28 @@ def CustomLoss(model_output, mask):
     # total_loss = loss_1 
     
     return total_loss
+
+
+
+if __name__ == '__main__':
+    import torch
+    x = torch.randn(2, 1, 256, 256)
+    model = models.vgg16(pretrained=True)
+    model.features[0] = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1) 
+    print(model.features(x).shape)
+
+    out = model.features(x)
+    print(model)
+    f1 = torch.sum(out, dim=[2, 3])
+    print(f1.shape)
+  
+
+
+    # loss = contrastive_loss(f1, f1)
+    # print(loss)
+
+
+
+    #model_x = nn.Sequential(model.features)
+    #print(model_x)
+
