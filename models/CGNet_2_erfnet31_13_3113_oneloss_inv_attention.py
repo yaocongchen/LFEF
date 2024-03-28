191795a2af1ae2af1f4cf822f71d14bba422aff7
yaocongchen <yaocongchen@outlook.com>
2024-03-28 17:48:46 +0800

    Fix input variable name and update joint feature calculation

--- models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
+++ models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
@@ -482,8 +482,8 @@ class ContextGuidedBlock(nn.Module):
         #joi_feat = torch.cat([loc, sur], 1)
         joi_feat = torch.cat([loc, sur, sur_4, sur_8], 1)  #  the joint feature
 
-        input = self.sigmoid(input)
-        joi_feat = joi_feat * input
+        input_sig = self.sigmoid(input)
+        joi_feat = joi_feat * input_sig
         joi_feat = self.conv3113(joi_feat)
 
         joi_feat = self.in_relu(joi_feat)
