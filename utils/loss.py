0d51f6f275081da7e8550b3861ee8bc9a7d082b1
yaocongchen <yaocongchen@outlook.com>
2024-04-23 12:11:41 +0800

    Refactor CustomLoss function in utils/loss.py to fix alpha value calculation

--- utils/loss.py
+++ utils/loss.py
@@ -207,7 +207,7 @@ def CustomLoss(*args, **kwargs):
     elif len(args) == 3:  # model_output, aux, 和 mask
         aux = args[1]
         loss_2 = smp_loss(aux, mask)
-        total_loss = loss_1 * (1 - alpha) + loss_2 * (alpha/2)
+        total_loss = loss_1 * (1 - alpha) + loss_2 * (alpha)
     else:
         raise ValueError("Unsupported number of arguments")
     
