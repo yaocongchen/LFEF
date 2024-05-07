6da903f51aae33afcb5f21421e0a0ca8773c64ce
yaocongchen <yaocongchen@outlook.com>
2024-05-07 15:28:02 +0800

    remove dataset

--- dataset/raw/rename.py
+++
@@ -1,7 +0,0 @@
-import glob
-from subprocess import call
-
-for filename in glob.glob("*.*"):
-	new_filename = filename.replace(' ','_')
-	call('rename \"'+filename+'\" '+new_filename, shell=True)
-	print('renamed file', filename)
