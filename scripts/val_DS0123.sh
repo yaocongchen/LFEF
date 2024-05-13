21d3943f669e7a432816d558387bd78936abaf48
yaocongchen <yaocongchen@outlook.com>
2024-05-13 16:56:41 +0800

    remove cd..

--- scripts/val_DS0123.sh
+++ scripts/val_DS0123.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 執行以下命令
-cd ..
+
 python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS01/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS01/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/best.pth
 python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS02/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS02/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/best.pth
 python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS03/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS03/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/best.pth
