6a2964037705bfc43ce85bd94046fe26696665f5
yaocongchen <yaocongchen@outlook.com>
2024-05-14 11:06:57 +0800

    base add INorm2d

--- models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
+++ models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
@@ -588,14 +588,14 @@ class AuxiliaryNetwork(nn.Module):
     def __init__(self, nIn, nOut, stride=1):
         super().__init__()
         # self.ea = ExternalAttention(d_model=nIn)
-        self.conv_layer1 = nn.Sequential(nn.Conv2d(nIn, 8, kernel_size=3, stride=stride, padding=1, bias=True),nn.ReLU())
-        self.conv_layer2 = nn.Sequential(nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1, bias=True), nn.ReLU())
-        self.conv_layer3 = nn.Sequential(nn.Conv2d(16, nOut, kernel_size=3, stride=1, padding=1, bias=True), nn.ReLU())
+        self.conv_layer1 = nn.Sequential(nn.Conv2d(nIn, 8, kernel_size=3, stride=stride, padding=1, bias=True),nn.InstanceNorm2d(8, affine=True),nn.ReLU())
+        self.conv_layer2 = nn.Sequential(nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1, bias=True),nn.InstanceNorm2d(16, affine=True), nn.ReLU())
+        self.conv_layer3 = nn.Sequential(nn.Conv2d(16, nOut, kernel_size=3, stride=1, padding=1, bias=True),nn.InstanceNorm2d(nOut, affine=True), nn.ReLU())
 
-        # self.avg_pool = nn.AvgPool2d(kernel_size=3, stride=1, padding = 1)
-        # self.max_pool = nn.MaxPool2d(kernel_size=3, stride=1, padding = 1)
+        self.avg_pool = nn.AvgPool2d(kernel_size=3, stride=1, padding = 1)
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=1, padding = 1)
 
-        # self.sigmoid = nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, input):
         # b, c, w, h = input.size()
@@ -692,13 +692,11 @@ class Net(nn.Module):
         self.brightness_adjustment = BrightnessAdjustment()
 
         self.level1_0 = ConvINReLU(3, 32, 3, 2)  # feature map size divided 2, 1/2
-        self.level1_1 = ConvINReLU(32, 32, 3, 1)
-        self.level1_2 = ConvINReLU(32, 32, 3, 1)
-        # self.level1_1 = non_bottleneck_1d(32, 1)
-        # self.level1_2 = non_bottleneck_1d(32, 2)
+        self.level1_1 = non_bottleneck_1d(32, 1)
+        self.level1_2 = non_bottleneck_1d(32, 2)
 
-        # self.max_pool = nn.MaxPool2d(3, stride=1, padding=1)
-        # self.avg_pool = nn.AvgPool2d(3, stride=1, padding=1)
+        self.max_pool = nn.MaxPool2d(3, stride=1, padding=1)
+        self.avg_pool = nn.AvgPool2d(3, stride=1, padding=1)
 
         self.sample1 = InputInjection(1)  # down-sample for Input Injection, factor=2
         self.sample2 = InputInjection(2)  # down-sample for Input Injiection, factor=4
