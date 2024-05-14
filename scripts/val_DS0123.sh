cc2984d3be08494f88a9d51ba6a7cc6c1a1d031a
yaocongchen <yaocongchen@outlook.com>
2024-05-14 16:01:38 +0800

    chore: Update validation script with variable for dataset path and model paths

--- scripts/val_DS0123.sh
+++ scripts/val_DS0123.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 # 執行以下命令
+#/home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data 設為一個變數
+path=/home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data
+host=./trained_models/best.pth
+server=./trained_models/server/best.pth
 
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS01/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS01/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/best.pth
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS02/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS02/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/best.pth
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS03/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS03/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/best.pth
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/Real/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/Real/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/best.pth
+python val.py -ti $path/DS01/images/ -tm $path/DS01/masks/ -m $host
+python val.py -ti $path/DS02/images/ -tm $path/DS02/masks/ -m $host
+python val.py -ti $path/DS03/images/ -tm $path/DS03/masks/ -m $host
+python val.py -ti $path/Real/images/ -tm $path/Real/masks/ -m $host
 
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS01/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS01/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/server/best.pth
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS02/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS02/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/server/best.pth
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS03/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/DS03/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/server/best.pth
-python val.py -ti /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/Real/images/ -tm /home/yaocong/Experimental/Dataset/SYN70K_dataset/testing_data/Real/masks/ -m /home/yaocong/Experimental/speed_smoke_segmentation/trained_models/server/best.pth
+python val.py -ti $path/DS01/images/ -tm $path/DS01/masks/ -m $server
+python val.py -ti $path/DS02/images/ -tm $path/DS02/masks/ -m $server
+python val.py -ti $path/DS03/images/ -tm $path/DS03/masks/ -m $server
+python val.py -ti $path/Real/images/ -tm $path/Real/masks/ -m $server
 
 
