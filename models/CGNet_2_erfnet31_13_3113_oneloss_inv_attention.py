d7741010782c21098320969928f773fad53bde73
yaocongchen <yaocongchen@outlook.com>
2024-05-15 11:17:58 +0800

    refactor: Update AttentionModule to concatenate input with average and maximum pooling outputs

--- models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
+++ models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
@@ -665,13 +665,13 @@ class AttentionModule(nn.Module):
         super().__init__()
         self.avg_pool = nn.AvgPool2d(3, stride=1, padding=1)
         self.max_pool = nn.MaxPool2d(3, stride=1, padding=1)
-        self.conv = nn.Conv2d(in_channels*2, in_channels, 1, bias=True)
+        self.conv = nn.Conv2d(in_channels*3, in_channels, 1, bias=True)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         avg_out = self.avg_pool(x)
         max_out = self.max_pool(x)
-        out = torch.cat([avg_out, max_out], dim=1)
+        out = torch.cat([x, avg_out, max_out], dim=1)
         out = self.conv(out)
         return self.sigmoid(out)
 
