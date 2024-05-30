d6aec415888cfac7d6ffcd272433b5cc43ca96df
yaocongchen <yaocongchen@outlook.com>
2024-05-30 14:06:48 +0800

    base_CGnet_ChannelWiseDilatedConv

--- models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
+++ models/CGNet_2_erfnet31_13_3113_oneloss_inv_attention.py
@@ -228,7 +228,7 @@ class DilatedConv(nn.Module):
 
 
 class ChannelWiseDilatedConv(nn.Module):
-    def __init__(self, nIn_ori, nOut_ori, kSize, stride=1, d=1):
+    def __init__(self, nIn, nOut, kSize, stride=1, d=1):
         """
         args:
            nIn: number of input channels
@@ -238,31 +238,26 @@ class ChannelWiseDilatedConv(nn.Module):
            d: dilation rate
         """
         super().__init__()
-        # nIn = int(nIn_ori / 2)
-        # nOut = int(nOut_ori / 2)
         padding = int((kSize - 1) / 2) * d
-        self.conv_3113 = nn.Sequential(
-            nn.Conv2d(
-                nIn_ori,
-                nIn_ori,
-                (kSize, 1),
-                stride=stride,
-                padding=(padding , 0),
-                groups=nIn_ori,
-                bias=True,
-                dilation=d,
-            ),
-            nn.Conv2d(
-                nIn_ori,
-                nOut_ori,
-                (1, kSize),
-                stride=stride,
-                padding=(0 , padding),
-                groups=nIn_ori,
-                bias=True,
-                dilation=d,
-            ),
+        self.conv = nn.Conv2d(
+            nIn,
+            nOut,
+            (kSize, kSize),
+            stride=stride,
+            padding=(padding, padding),
+            groups=nIn,
+            bias=False,
+            dilation=d,
         )
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
 
         # self.conv_1331 = nn.Sequential(
         #     nn.Conv2d(
@@ -294,31 +289,6 @@ class ChannelWiseDilatedConv(nn.Module):
         # self.sigmoid = nn.Sigmoid()
 
 
-    def forward(self, input):
-        """
-        args:
-           input: input feature map
-           return: transformed feature map
-        """
-        output = self.conv_3113(input)
-
-        # x1, x2 = channel_split(input)
-        # output_3113 = self.conv_3113(x1)
-        # output_1331 = self.conv_1331(x2)
-        # output = torch.cat([output_3113, output_1331], 1)
-        # output = self.conv_1x1(output)
-
-        # mix_input = self.avg_pool(input) + self.max_pool(input)
-        # mix_input = self.conv_1x1_ori(mix_input)
-        # mix_input = self.sigmoid(mix_input)
-
-        # output_mul_input = output * mix_input
-
-        # output =  output + output_mul_input
-
-        # output = channel_shuffle(output, 2)
-
-        return output
 
 
 class FGlo(nn.Module):
